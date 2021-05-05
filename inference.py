cc000088cb9257bc9ebc1187bb815bcde581624e
newsm5403 <newsm5403@naver.com>
2021-05-05 16:37:14 +0900

    initial commit

--- inference.py
+++ inference.py
@@ -1,21 +1,23 @@
-import cv2 
+import cv2
 import mediapipe as mp
 import math
-import numpy as np 
-import os 
+import numpy as np
+import os
 import time
 import torch
 
+
 def distance(p1, p2):
-    ''' Calculate distance between two points
-    :param p1: First Point 
+    ''' 두 점의 거리를 계산한다
+    :param p1: First Point
     :param p2: Second Point
-    :return: Euclidean distance between the points. (Using only the x and y coordinates).
+    :return: 유클리드 거리를 계산한다
     '''
-    return (((p1[:2] - p2[:2])**2).sum())**0.5
+    return (((p1[:2] - p2[:2]) ** 2).sum()) ** 0.5
+
 
 def eye_aspect_ratio(landmarks, eye):
-    ''' Calculate the ratio of the eye length to eye width. 
+    ''' 눈 너비와 눈 높이의 비(ratio)를 계산한다
     :param landmarks: Face Landmarks returned from FaceMesh MediaPipe model
     :param eye: List containing positions which correspond to the eye
     :return: Eye aspect ratio value
@@ -26,13 +28,15 @@ def eye_aspect_ratio(landmarks, eye):
     D = distance(landmarks[eye[0][0]], landmarks[eye[0][1]])
     return (N1 + N2 + N3) / (3 * D)
 
+
 def eye_feature(landmarks):
-    ''' Calculate the eye feature as the average of the eye aspect ratio for the two eyes
+    ''' 두 눈 종횡비의 평균으로 눈 특징을 계산한다
     :param landmarks: Face Landmarks returned from FaceMesh MediaPipe model
     :return: Eye feature value
     '''
     return (eye_aspect_ratio(landmarks, left_eye) + \
-    eye_aspect_ratio(landmarks, right_eye))/2
+            eye_aspect_ratio(landmarks, right_eye)) / 2
+
 
 def mouth_feature(landmarks):
     ''' Calculate mouth feature as the ratio of the mouth length to mouth width
@@ -43,35 +47,38 @@ def mouth_feature(landmarks):
     N2 = distance(landmarks[mouth[2][0]], landmarks[mouth[2][1]])
     N3 = distance(landmarks[mouth[3][0]], landmarks[mouth[3][1]])
     D = distance(landmarks[mouth[0][0]], landmarks[mouth[0][1]])
-    return (N1 + N2 + N3)/(3*D)
+    return (N1 + N2 + N3) / (3 * D)
+
 
 def pupil_circularity(landmarks, eye):
-    ''' Calculate pupil circularity feature.
+    ''' 동공의 원형(circularity feature)을 계산한다.
     :param landmarks: Face Landmarks returned from FaceMesh MediaPipe model
     :param eye: List containing positions which correspond to the eye
     :return: Pupil circularity for the eye coordinates
     '''
     perimeter = distance(landmarks[eye[0][0]], landmarks[eye[1][0]]) + \
-            distance(landmarks[eye[1][0]], landmarks[eye[2][0]]) + \
-            distance(landmarks[eye[2][0]], landmarks[eye[3][0]]) + \
-            distance(landmarks[eye[3][0]], landmarks[eye[0][1]]) + \
-            distance(landmarks[eye[0][1]], landmarks[eye[3][1]]) + \
-            distance(landmarks[eye[3][1]], landmarks[eye[2][1]]) + \
-            distance(landmarks[eye[2][1]], landmarks[eye[1][1]]) + \
-            distance(landmarks[eye[1][1]], landmarks[eye[0][0]])
+                distance(landmarks[eye[1][0]], landmarks[eye[2][0]]) + \
+                distance(landmarks[eye[2][0]], landmarks[eye[3][0]]) + \
+                distance(landmarks[eye[3][0]], landmarks[eye[0][1]]) + \
+                distance(landmarks[eye[0][1]], landmarks[eye[3][1]]) + \
+                distance(landmarks[eye[3][1]], landmarks[eye[2][1]]) + \
+                distance(landmarks[eye[2][1]], landmarks[eye[1][1]]) + \
+                distance(landmarks[eye[1][1]], landmarks[eye[0][0]])
     area = math.pi * ((distance(landmarks[eye[1][0]], landmarks[eye[3][1]]) * 0.5) ** 2)
-    return (4*math.pi*area)/(perimeter**2)
+    return (4 * math.pi * area) / (perimeter ** 2)
+
 
 def pupil_feature(landmarks):
-    ''' Calculate the pupil feature as the average of the pupil circularity for the two eyes
+    ''' 두 눈에 대한 동공 원형의 평균으로 동공 형상을 계산한다.
     :param landmarks: Face Landmarks returned from FaceMesh MediaPipe model
     :return: Pupil feature value
     '''
     return (pupil_circularity(landmarks, left_eye) + \
-        pupil_circularity(landmarks, right_eye))/2
+            pupil_circularity(landmarks, right_eye)) / 2
+
 
 def run_face_mp(image):
-    ''' Get face landmarks using the FaceMesh MediaPipe model. 
+    ''' Get face landmarks using the FaceMesh MediaPipe model.
     Calculate facial features using the landmarks.
     :param image: Image for which to get the face landmarks
     :return: Feature 1 (Eye), Feature 2 (Mouth), Feature 3 (Pupil), \
@@ -80,7 +87,7 @@ def run_face_mp(image):
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = face_mesh.process(image)
-    
+
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
@@ -88,24 +95,25 @@ def run_face_mp(image):
         landmarks_positions = []
         # assume that only face is present in the image
         for _, data_point in enumerate(results.multi_face_landmarks[0].landmark):
-            landmarks_positions.append([data_point.x, data_point.y, data_point.z]) # saving normalized landmark positions
+            landmarks_positions.append(
+                [data_point.x, data_point.y, data_point.z])  # saving normalized landmark positions
         landmarks_positions = np.array(landmarks_positions)
         landmarks_positions[:, 0] *= image.shape[1]
         landmarks_positions[:, 1] *= image.shape[0]
 
         # draw face mesh over image
         for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec)
+            mp_drawing.draw_landmarks(
+                image=image,
+                landmark_list=face_landmarks,
+                connections=mp_face_mesh.FACE_CONNECTIONS,
+                landmark_drawing_spec=drawing_spec,
+                connection_drawing_spec=drawing_spec)
 
         ear = eye_feature(landmarks_positions)
         mar = mouth_feature(landmarks_positions)
         puc = pupil_feature(landmarks_positions)
-        moe = mar/ear
+        moe = mar / ear
     else:
         ear = -1000
         mar = -1000
@@ -114,8 +122,9 @@ def run_face_mp(image):
 
     return ear, mar, puc, moe, image
 
+
 def calibrate(calib_frame_count=25):
-    ''' Perform clibration. Get features for the neutral position.
+    ''' Perform calibration. 이미지의 중간 지점에서 특징들을 추출합니다.
     :param calib_frame_count: Image frames for which calibration is performed. Default Vale of 25.
     :return: Normalization Values for feature 1, Normalization Values for feature 2, \
         Normalization Values for feature 3, Normalization Values for feature 4
@@ -132,21 +141,21 @@ def calibrate(calib_frame_count=25):
             print("Ignoring empty camera frame.")
             continue
 
-        ear, mar,puc, moe, image = run_face_mp(image)
+        ear, mar, puc, moe, image = run_face_mp(image)
         if ear != -1000:
             ears.append(ear)
             mars.append(mar)
             pucs.append(puc)
             moes.append(moe)
 
-        cv2.putText(image, "Calibration", (int(0.02*image.shape[1]), int(0.14*image.shape[0])),
-                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
+        cv2.putText(image, "Calibration", (int(0.02 * image.shape[1]), int(0.14 * image.shape[0])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
         cv2.imshow('MediaPipe FaceMesh', image)
         if cv2.waitKey(5) & 0xFF == ord("q"):
             break
         if len(ears) >= calib_frame_count:
             break
-    
+
     cv2.destroyAllWindows()
     cap.release()
     ears = np.array(ears)
@@ -154,10 +163,11 @@ def calibrate(calib_frame_count=25):
     pucs = np.array(pucs)
     moes = np.array(moes)
     return [ears.mean(), ears.std()], [mars.mean(), mars.std()], \
-        [pucs.mean(), pucs.std()], [moes.mean(), moes.std()]
+           [pucs.mean(), pucs.std()], [moes.mean(), moes.std()]
+
 
 def get_classification(input_data):
-    ''' Perform classification over the facial  features.
+    ''' 얼굴 특징들을 classification 해준다
     :param input_data: List of facial features for 20 frames
     :return: Alert / Drowsy state prediction
     '''
@@ -172,18 +182,19 @@ def get_classification(input_data):
     preds = torch.sigmoid(model(model_input)).gt(0.5).int().data.numpy()
     return int(preds.sum() >= 5)
 
+
 def infer(ears_norm, mars_norm, pucs_norm, moes_norm):
     ''' Perform inference.
     :param ears_norm: Normalization values for eye feature
     :param mars_norm: Normalization values for mouth feature
     :param pucs_norm: Normalization values for pupil feature
-    :param moes_norm: Normalization values for mouth over eye feature. 
+    :param moes_norm: Normalization values for mouth over eye feature.
     '''
     ear_main = 0
     mar_main = 0
     puc_main = 0
     moe_main = 0
-    decay = 0.9 # use decay to smoothen the noise in feature values
+    decay = 0.9  # use decay to smoothen the noise in feature values
 
     label = None
 
@@ -199,26 +210,26 @@ def infer(ears_norm, mars_norm, pucs_norm, moes_norm):
 
         ear, mar, puc, moe, image = run_face_mp(image)
         if ear != -1000:
-            ear = (ear - ears_norm[0])/ears_norm[1]
-            mar = (mar - mars_norm[0])/mars_norm[1]
-            puc = (puc - pucs_norm[0])/pucs_norm[1]
-            moe = (moe - moes_norm[0])/moes_norm[1]
+            ear = (ear - ears_norm[0]) / ears_norm[1]
+            mar = (mar - mars_norm[0]) / mars_norm[1]
+            puc = (puc - pucs_norm[0]) / pucs_norm[1]
+            moe = (moe - moes_norm[0]) / moes_norm[1]
             if ear_main == -1000:
                 ear_main = ear
                 mar_main = mar
                 puc_main = puc
                 moe_main = moe
             else:
-                ear_main = ear_main*decay + (1-decay)*ear
-                mar_main = mar_main*decay + (1-decay)*mar
-                puc_main = puc_main*decay + (1-decay)*puc
-                moe_main = moe_main*decay + (1-decay)*moe
+                ear_main = ear_main * decay + (1 - decay) * ear
+                mar_main = mar_main * decay + (1 - decay) * mar
+                puc_main = puc_main * decay + (1 - decay) * puc
+                moe_main = moe_main * decay + (1 - decay) * moe
         else:
             ear_main = -1000
             mar_main = -1000
             puc_main = -1000
             moe_main = -1000
-        
+
         if len(input_data) == 20:
             input_data.pop(0)
         input_data.append([ear_main, mar_main, puc_main, moe_main])
@@ -227,53 +238,52 @@ def infer(ears_norm, mars_norm, pucs_norm, moes_norm):
         if frame_before_run >= 15 and len(input_data) == 20:
             frame_before_run = 0
             label = get_classification(input_data)
-            print ('got label ', label)
-        
-        cv2.putText(image, "EAR: %.2f" %(ear_main), (int(0.02*image.shape[1]), int(0.07*image.shape[0])),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        cv2.putText(image, "MAR: %.2f" %(mar_main), (int(0.27*image.shape[1]), int(0.07*image.shape[0])),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        cv2.putText(image, "PUC: %.2f" %(puc_main), (int(0.52*image.shape[1]), int(0.07*image.shape[0])),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        cv2.putText(image, "MOE: %.2f" %(moe_main), (int(0.77*image.shape[1]), int(0.07*image.shape[0])),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+
+        cv2.putText(image, "EAR: %.2f" % (ear_main), (int(0.02 * image.shape[1]), int(0.07 * image.shape[0])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(image, "MAR: %.2f" % (mar_main), (int(0.27 * image.shape[1]), int(0.07 * image.shape[0])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(image, "PUC: %.2f" % (puc_main), (int(0.52 * image.shape[1]), int(0.07 * image.shape[0])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(image, "MOE: %.2f" % (moe_main), (int(0.77 * image.shape[1]), int(0.07 * image.shape[0])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
         if label is not None:
             if label == 0:
                 color = (0, 255, 0)
             else:
                 color = (0, 0, 255)
-            cv2.putText(image, "%s" %(states[label]), (int(0.02*image.shape[1]), int(0.2*image.shape[0])),
+            cv2.putText(image, "%s" % (states[label]), (int(0.02 * image.shape[1]), int(0.2 * image.shape[0])),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2)
 
         cv2.imshow('MediaPipe FaceMesh', image)
         if cv2.waitKey(5) & 0xFF == ord("q"):
             break
-    
+
     cv2.destroyAllWindows()
     cap.release()
 
 
-right_eye = [[33, 133], [160, 144], [159, 145], [158, 153]] # right eye landmark positions
-left_eye = [[263, 362], [387, 373], [386, 374], [385, 380]] # left eye landmark positions
-mouth = [[61, 291], [39, 181], [0, 17], [269, 405]] # mouth landmark coordinates
+right_eye = [[33, 133], [160, 144], [159, 145], [158, 153]]  # right eye landmark positions
+left_eye = [[263, 362], [387, 373], [386, 374], [385, 380]]  # left eye landmark positions
+mouth = [[61, 291], [39, 181], [0, 17], [269, 405]]  # mouth landmark coordinates
 states = ['alert', 'drowsy']
 
 # Declaring FaceMesh model
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
     min_detection_confidence=0.3, min_tracking_confidence=0.8)
-mp_drawing = mp.solutions.drawing_utils 
+mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-model_lstm_path = 'models\clf_lstm_jit6.pth'
+model_lstm_path = '\\Users\\user\\Drowsiness-Detection-Mediapipe\\models\\clf_lstm_jit6.pth'
 model = torch.jit.load(model_lstm_path)
 model.eval()
 
-print ('Starting calibration. Please be in neutral state')
+print('Starting calibration. Please be in neutral state')
 time.sleep(1)
 ears_norm, mars_norm, pucs_norm, moes_norm = calibrate()
 
-print ('Starting main application')
+print('Starting main application')
 time.sleep(1)
 infer(ears_norm, mars_norm, pucs_norm, moes_norm)
 
